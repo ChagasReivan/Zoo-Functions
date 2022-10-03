2730c608489c6e6ed96052bb8f1dc0e3237aa58a
Reivan Chagas <chagasreivan@gmail.com>
2022-10-03 20:03:17 -0300

    adiciona exercicios

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const { employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const encarregado = employees.find((employee) => employee.id === id);
+  const primeiroAnimal = encarregado.responsibleFor[0];
+  const especieEspecifica = species.find((specie) => primeiroAnimal.includes(specie.id));
+  const objetoAnimais = especieEspecifica.residents;
+  let idade = 0;
+  let maisVelho;
+  objetoAnimais.forEach((resident) => {
+    if (idade < resident.age) {
+      idade = resident.age;
+      maisVelho = resident;
+    }
+  });
+  const acheiMisseravel = Object.values(maisVelho);
+  return acheiMisseravel;
+
 }
+
+console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,51 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const total = {
+  Tuesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
+  },
+  Wednesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'],
+  },
+  Thursday: {
+    officeHour: 'Open from 10am until 8pm',
+    exhibition: ['lions', 'otters', 'frogs', 'snakes', 'giraffes'],
+  },
+  Friday: {
+    officeHour: 'Open from 10am until 8pm',
+    exhibition: ['tigers', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes'],
+  },
+  Saturday: {
+    officeHour: 'Open from 8am until 10pm',
+    exhibition: [
+      'lions', 'tigers',
+      'bears', 'penguins',
+      'otters', 'frogs',
+      'snakes', 'elephants',
+    ],
+  },
+  Sunday: {
+    officeHour: 'Open from 8am until 8pm',
+    exhibition: ['lions', 'bears', 'penguins', 'snakes', 'elephants'],
+  },
+  Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+};
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const animalName = species.map((specie2) => specie2.name);
+  if (animalName.includes(scheduleTarget)) {
+    return species.find((specie) => specie.name === scheduleTarget).availability;
+  }
+  if (Object.keys(total).includes(scheduleTarget)) {
+    return { [scheduleTarget]: total[scheduleTarget] };
+  }
+  return total;
 }
+
+console.log(getSchedule('Saturday'));
 
 module.exports = getSchedule;
