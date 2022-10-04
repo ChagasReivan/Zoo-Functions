c597b3c4bf74d51260653f43f7128a16f4a41112
Reivan Chagas <chagasreivan@gmail.com>
2022-10-04 19:02:16 -0300

    80% tomalhe

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  const obj = {};
+  if (animal === undefined) {
+    species.forEach((specie) => {
+      obj[specie.name] = specie.residents.length;
+    });
+    return obj;
+  }
+  const totalAnimais = species.find((spc) => spc.name === animal.specie).residents;
+  if (Object.keys(animal).includes('sex')) {
+    const especificioAnimais = totalAnimais.filter((e) => e.sex === animal.sex);
+    return especificioAnimais.length;
+  }
+  return totalAnimais.length;
 }
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
